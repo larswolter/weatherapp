6ea26679ca239cbe1cad7b212caaab9c4a24b5cd
Lars Wolter <lars@larswolter.de>
2021-07-09 02:20:50 +0200

    Adde rain and wind and range selection

--- imports/ui/History.js
+++ imports/ui/History.js
@@ -1,23 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { SensorReadings } from '../api/sensorData';
-import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, Line, LineChart, Legend, ResponsiveContainer, Label } from 'recharts';
+import { XAxis, YAxis, Tooltip, CartesianGrid, Line, LineChart, Legend, ResponsiveContainer } from 'recharts';
+import { makeStyles } from '@material-ui/styles';
+import { Paper, Slider } from '@material-ui/core';
 import dayjs from 'dayjs';
+import { Box } from '@material-ui/core';
+import debounce from '@material-ui/core/utils/debounce';
+
+const useStyles = makeStyles(theme => {
+  return {
+    slider: {
+      position: 'fixed',
+      padding: theme.spacing(),
+      bottom: 55,
+      left: 0,
+      right: 0
+    }
+  }
+});
 
 const History = () => {
+  const classes = useStyles();
+  const [hourRange, setHourRange] = useState([0, 4]);
+
+  
   useEffect(() => {
-    const sub = Meteor.subscribe('sensorReadings', 100);
-    return () => sub.stop();
-  }, []);
+    const start = hourRange[0] && dayjs().subtract(hourRange[0], 'hours').toDate();
+    const end = hourRange[1] && dayjs().subtract(hourRange[1], 'hours').toDate()
+    const sub = Meteor.subscribe('sensorReadings', start, end);
+    /*
+    Meteor.call('sensorAggregation', start, end,(err,res)=>{
+      console.log(err,res);
+    });
+    */
+    return () => {
+      sub.stop();
+    }
+  }, [hourRange]);
   const sensorReadings = useTracker(() => {
     return SensorReadings.find({}, { sort: { date: -1 } }).fetch();
   });
-  console.log(sensorReadings);
+  const hour = dayjs().hour() + 1;
   return (
     <>
       <ResponsiveContainer width="100%" height={200}>
-        <LineChart width={730} height={250} data={sensorReadings.map(sensor => {
+        <LineChart syncId="anyId" width={730} height={250} data={sensorReadings.map(sensor => {
           return {
             name: dayjs(sensor.date).format('DD.MM HH:mm'),
             Außen: (Number(sensor.parsed.tempf) - 32) * 5 / 9,
@@ -35,7 +64,7 @@ const History = () => {
         </LineChart>
       </ResponsiveContainer>
       <ResponsiveContainer width="100%" height={200}>
-        <LineChart width={730} height={250} data={sensorReadings.map(sensor => {
+        <LineChart syncId="anyId" width={730} height={250} data={sensorReadings.map(sensor => {
           return {
             name: dayjs(sensor.date).format('DD.MM HH:mm'),
             Außen: Number(sensor.parsed.humidity),
@@ -46,7 +75,7 @@ const History = () => {
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <XAxis dataKey="name" />
           <Legend verticalAlign="top" height={36} />
-          <YAxis type="number" unit="%" domain={[0, 100]} for label={{ value: 'Feuchtigkeit', angle: -90, position: 'insideLeft' }}/>
+          <YAxis type="number" unit="%" domain={[0, 100]} for label={{ value: 'Feuchtigkeit', angle: -90, position: 'insideLeft' }} />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip formatter={(value) => value.toFixed(2) + '°'} />
           <Line type="monotone" dataKey="Außen" dot={false} stroke="#000088" />
@@ -54,7 +83,49 @@ const History = () => {
           <Line type="monotone" dataKey="Boden" dot={false} stroke="#448844" />
         </LineChart>
       </ResponsiveContainer>
-
+      <ResponsiveContainer width="100%" height={200} >
+        <LineChart syncId="anyId" width={730} height={250} data={sensorReadings.map(sensor => {
+          return {
+            name: dayjs(sensor.date).format('DD.MM HH:mm'),
+            Regen: Number(sensor.parsed.rainratein),
+            Wind: Number(sensor.parsed.windspeedmph),
+            Sonnenschein: Number(sensor.parsed.solarradiation) / 100,
+          }
+        })}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <XAxis dataKey="name" />
+          <Legend verticalAlign="top" height={36} />
+          <YAxis type="number" width={25} allowDecimals={false} for yAxisId="left" />
+          <YAxis type="number" width={35} axisLine={{ stroke: "#000088" }} for yAxisId="right" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip formatter={(value) => value.toFixed(2) + '°'} />
+          <Line type="monotone" yAxisId="right" dataKey="Regen" dot={false} stroke="#000088" />
+          <Line type="monotone" yAxisId="left" dataKey="Wind" dot={false} stroke="#aaaaaa" />
+          <Line type="monotone" yAxisId="left" dataKey="Sonnenschein" dot={false} stroke="#ff8800" />
+        </LineChart>
+      </ResponsiveContainer>
+      <Box height={100}>&nbsp;</Box>
+      <Paper className={classes.slider}>
+        <Slider
+          value={hourRange}
+          aria-labelledby="discrete-slider-custom"
+          step={1}
+          onChange={(evt, value) => {
+            if((hourRange[0] !== value[0]) && (value[1] - value[0] > 12) ) {
+              setHourRange([value[0],value[0]+12]);
+            } else if((hourRange[1] !== value[1]) && (value[1] - value[0] > 12) ) {
+              setHourRange([value[1]-12,value[1]]);
+            } else
+            setHourRange(value);
+          }}
+          valueLabelFormat={(value) => dayjs().subtract(value, 'hours').format('HH') + 'Uhr'}
+          valueLabelDisplay="on"
+          marks={[24 - hour, 48 - hour, 72 - hour, 96 - hour, 120 - hour, 144 - hour].map(value => ({
+            value,
+            label: dayjs().subtract(value, 'hours').format('DD.MM.')
+          }))}
+        />
+      </Paper>
     </>);
 };
 
--- server/main.js
+++ server/main.js
@@ -1,18 +1,54 @@
 import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
+import { check, Match } from 'meteor/check';
 import { WebApp } from 'meteor/webapp';
-import { SensorReadings } from '/imports/api/sensorData';
+import { SensorReadings } from '../imports/api/sensorData';
 
-Meteor.publish('sensorReadings', function (limit = 1) {
-  check(limit, Number);
-  return SensorReadings.find({}, { sort: { date: -1 }, limit });
+const SENSOR_FIELDS = {
+  date:1,
+  'parsed.tempf':1,
+  'parsed.tempinf':1,
+  'parsed.humidity':1,
+  'parsed.humidityin':1,
+  'parsed.soilmoisture1':1,
+  'parsed.rainratein':1,
+  'parsed.windspeedmph':1,
+  'parsed.solarradiation':1,
+};
+
+Meteor.publish('sensorReadings', function (start, end) {
+  //check(start, Match.Maybe(Date));
+  //check(end, Match.Maybe(Date));
+  const search = {};
+  if (start && end) search.date = { $lte: start, $gte: end };
+  else if (start) search.date = { $lte: start };
+  else if (end) search.date = { $gte: end };
+  else return SensorReadings.find(search, { fields: SENSOR_FIELDS, sort: { date: -1 }, limit: 1 });
+  return SensorReadings.find(search, { fields: SENSOR_FIELDS, sort: { date: -1 }, limit: 800 });
 });
-
+Meteor.methods({
+  async sensorAggregation(start,end) {
+    const search = {};
+    if (start && end) search.date = { $lte: start, $gte: end };
+    else if (start) search.date = { $lte: start };
+    else if (end) search.date = { $gte: end };
+    return await SensorReadings.rawCollection().aggregate([
+      {$match:search},
+      {$bucketAuto: {
+        $groupBy:'$date',
+        buckets: 20,
+        output: {
+          tempinf: {$average: '$parsed.tempinf'},
+          tempf: {$average: '$parsed.tempf'},
+          date: {$first:'$date'}
+        }
+      }}
+    ]).toArray();
+  }
+})
 WebApp.connectHandlers.use('/weatherinput', (request, response) => {
   let raw = '';
   request.on('data', (chunk) => {
     raw += chunk.toString();
-    console.log(chunk.toString());
   });
   request.on('end', Meteor.bindEnvironment((chunk) => {
     const parsed = {};
